134f5bad5258bdf3d17a1b1718fb693c6e65c11c
Tony H.Vu <tony.v95@hotmail.com>
2024-02-22 21:34:08 +0100

    Added MultiTouch

--- Assets/Scripts/BallLauncher/BallHandler.cs
+++ Assets/Scripts/BallLauncher/BallHandler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private GameObject ballPrefab;
@@ -23,12 +25,23 @@ public class BallHandler : MonoBehaviour
         SpawnNewBall();
     }
 
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (currentBallRb2d == null) return;
 
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        if (Touch.activeTouches.Count == 0)
         {
             if(isDragging)
             {
@@ -39,12 +52,25 @@ public class BallHandler : MonoBehaviour
         }
 
         isDragging = true;
-
         //enables rigidbody kinematic
         currentBallRb2d.isKinematic = true;
 
-        //acquire touch screen position
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector2 touchPosition = new Vector2(); //Right here, it is just (0,0) because of zero touches.
+         
+        int effectiveTouchCount = 0; //needed to calculate touch points.
+        foreach (Touch touch in Touch.activeTouches)
+        {
+            if (new Rect(1, 1, Screen.width - 1, Screen.height - 1).Contains(touch.screenPosition)) //needed to calculate touch points.
+            {
+                touchPosition += touch.screenPosition;
+                effectiveTouchCount++;
+            }
+        }
+        touchPosition /= effectiveTouchCount; //This get the centrepoints of all the touches.
+
+        ////acquire touch screen position
+        //Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
         //converts screen to world position
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
